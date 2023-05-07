716b0477955d72fa62a6f0dd7d8650c681d9f6d9
Alfred <alfred.sherlock@jointheleague.org>
2023-05-07 11:52:52 -0700

    initial

---
+++ src/main/java/org/jointheleague/api/HulkBobster/HulkBobsterApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.HulkBobster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkBobsterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkBobsterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jointheleague/api/HulkBobster/HulkBobsterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.HulkBobster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkBobsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
